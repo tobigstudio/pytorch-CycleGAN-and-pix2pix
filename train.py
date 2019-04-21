711a3ff66711339ba0e7043e0444484fa9656d7a
krlee <krlee407@gmail.com>
2019-04-21 18:49:06 +0900

    visualizer off

--- train.py
+++ train.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
                 losses = model.get_current_losses()
                 t_comp = (time.time() - iter_start_time) / opt.batch_size
 				#visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
-                if opt.display_id > 0:
+				#if opt.display_id > 0:
 					#visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
 
             if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
